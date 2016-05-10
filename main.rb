6fd79ff96a5bb90e6428a6c82fce6ae6ea22b536
Arian <arian2k@gmail.com>
2016-05-10 03:40:47 +0300

    added #showcase and #buy in Product class (25-3)

--- main.rb
+++ main.rb
@@ -8,6 +8,8 @@ require_relative 'movie.rb'
 require_relative 'music.rb'
 require_relative 'book.rb'
 
+products = []
+
 leon = Movie.new(550, 4)
 leon.update(:title => "Леон", :producer_name => "Люк Бессон", :year => "1994")
 
@@ -17,6 +19,10 @@ tom.update(:title => "Приключения Тома Сойера", :author_nam
 judas = Music.new(590, 10)
 judas.update(:album_name => "Turbo", :artist_name => "Judas Priest", :genre => "Heavy Metal")
 
-puts leon.show
-puts tom.show
-puts judas.show
+products << leon
+products << tom
+products << judas
+
+Product.showcase(products)
+
+products[0].buy
--- product.rb
+++ product.rb
@@ -17,4 +17,22 @@ class Product
   def show
     "#{info} - #{price} руб. [осталось: #{@product_quantity}]"
   end
+
+  def self.showcase(products)
+    puts "Что хотите купить?"
+    puts
+    products.each_with_index { |item, idx| puts "#{idx}: #{item.show}" }
+    puts
+  end
+
+  def buy
+    if @product_quantity > 0
+      puts "Вы купили товар: #{info}"
+      @product_quantity -= 1
+      price
+    else
+      puts "Товар закончился"
+      0
+    end
+  end
 end
